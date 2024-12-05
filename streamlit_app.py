e315848bb77019f761b53c30ad5e8cf6254eb84d
AA2030District <159567386+AA2030District@users.noreply.github.com>
2024-12-05 11:10:58 +0000

    double files

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,5 @@ import xmltodict
 st.title("2030 District Constellation Uploader")
 st.write(
     "go my minions!")
-uploaded_file = st.file_uploader("Upload your excel file", type="xls")
+uploaded_file = st.file_uploader("Upload your download of properties you wish to address here", type="xlsx")
+uploaded_file2 = st.file_uploader("Upload your constellation file here", type="xlsx")
