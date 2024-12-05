accfed790c0f531be13faced7e1151eb6e6802df
AA2030District <159567386+AA2030District@users.noreply.github.com>
2024-12-05 10:29:43 +0000

    can u work?

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("2030 District Constellation Uploader")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "go"
 )
