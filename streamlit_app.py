ca2a40374cb57364ee7aecd04bc417645223f210
AA2030District <159567386+AA2030District@users.noreply.github.com>
2024-12-05 10:48:52 +0000

    added requirements

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import pandas as pd
+import requests
+from requests.auth import HTTPBasicAuth 
+import xml.etree.ElementTree as et
+import xmltodict
 
 st.title("2030 District Constellation Uploader")
 st.write(
-    "go"
+    "go my minions!"
 )
