b2e05bd094a9c499d45a5b71ffd90284a8f487b6
AA2030District <159567386+AA2030District@users.noreply.github.com>
2024-12-09 03:13:44 +0000

    hey can this work

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,11 +8,29 @@ import xmltodict
 st.title("2030 District Constellation Uploader")
 st.write(
     "go my minions!")
-uploaded_file = st.file_uploader("Upload your download of properties you wish to address here", type="xlsx")
-uploaded_file2 = st.file_uploader("Upload your constellation file here", type="xlsx")
+espmfile   = st.file_uploader("Upload your download of properties you wish to address here", type="xlsx")
+inputfile = st.file_uploader("Upload your constellation file here", type="xlsx")
 
 def espmidmatcher():
-    st.write("this button works!")
+    esdf = pd.read_excel(espmfile,'Meters')
+    esdf = esdf.iloc[4:]
+    new_header = esdf.iloc[0] #grab the first row for the header
+    esdf = esdf[1:] #take the data less the header row
+    esdf.columns = new_header #set the header row as the df header
+    espmdict={}
+    counter=5
+    newdictionary={}
+    failedlist=[]
+    #CUSTOM ID SHOULD BE FIRST OEN!
+    counter=4   
+    for property in esdf.iloc:
+        espmdict.update({property['Custom Meter ID 1 Value']:[property['Portfolio Manager ID'],property['Portfolio Manager Meter ID']]})
+        counter+=1
+    st.write(espmdict)
+    return espmdict
+
+
 
 if st.button("Run Program"):
     espmidmatcher()
+
